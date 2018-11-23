21e096433f463891fff5123cc96d34d19a5acb31
wuzht <770333760@qq.com>
2018-11-23 23:35:14 +0800

    cm

--- UDPtest/UDPClient.py
+++
@@ -1,9 +0,0 @@
-from socket import *
-serverName = 'localhost'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence:')
-clientSocket.sendto(message.encode('UTF-8'), (serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode('UTF-8'))
-clientSocket.close()
