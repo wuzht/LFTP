ed1518f47575cbe2578963a3e62cee3eb9b79152
wuzht <770333760@qq.com>
2018-11-23 23:30:55 +0800

    实现了lsend和lget的基本功能

--- LFTPClient.py
+++ LFTPClient.py
@@ -7,8 +7,35 @@ SERVER_PORT = 12000
 CLIENT_FOLDER = 'ClientFiles/'   # 接收文件夹
 
 
-def lsend(client_socket, server_addr, file_name):
-    print("LFTP lsend", server_addr, file_name)
+def lsend(client_socket, server_address, large_file_name):
+    print("LFTP lsend", server_address, large_file_name)
+    # 发送数据包次数计数
+    pkt_count = 0
+    # 模式rb 以二进制格式打开一个文件用于只读。文件指针将会放在文件的开头。
+    file_to_send = open(CLIENT_FOLDER + large_file_name, 'rb')
+
+    # 发送ACK 注意要做好所有准备(比如打开文件)后才向服务端发送ACK
+    client_socket.sendto('ACK'.encode('utf-8'), server_address)
+
+    # 等待服务端的接收允许
+    message, server_address = client_socket.recvfrom(BUF_SIZE)
+    print('来自', server_address, '的数据是: ', message.decode('utf-8'))
+
+    # 用缓冲区循环发送文件
+    while True:
+        data = file_to_send.read(BUF_SIZE)
+        if str(data) != "b''":  # b''表示文件读完
+            client_socket.sendto(data, server_address)
+        else:
+            # 把'end'发送给客户端，表示文件已发送完毕
+            client_socket.sendto('end'.encode('utf-8'), server_address)
+            break
+        # 等待服务端ACK
+        data, server_address = client_socket.recvfrom(BUF_SIZE)
+        # print('接受自 ', client_address, '收到数据为 : ', data.decode('utf-8'))
+        pkt_count += 1
+
+    print(large_file_name, '发送完毕，发送数据包的数量：' + str(pkt_count))
 
 
 def lget(client_socket, server_address, large_file_name):
@@ -39,6 +66,10 @@ def lget(client_socket, server_address, large_file_name):
 
 
 def connection_request(client_socket, server_addr, cmd, large_file_name):
+    # 若要发送的文件不存在，退出程序
+    if cmd == 'lsend' and (os.path.exists(CLIENT_FOLDER + large_file_name) is False):
+        exit(2)
+
     # 三次握手
     # 连接请求，格式为[lsend|lget]#large_file_name，因此文件命名不允许含有#
     client_socket.sendto((cmd + '#' + large_file_name).encode('utf-8'), server_addr)
@@ -53,12 +84,15 @@ def connection_request(client_socket, server_addr, cmd, large_file_name):
     # 注意要做好所有准备(比如创建文件)后才向服务端发送ACK
     if cmd == 'lget':
         lget(client_socket, server_address, large_file_name)
+    elif cmd == 'lsend':
+        lsend(client_socket, server_address, large_file_name)
 
 
 def read_command(client_socket):
     print('请输入命令: LFTP [lsend | lget] server_addr large_file_name')
     pattern = re.compile(r"(LFTP) (lsend|lget) (\S+) (\S+)")
     # LFTP lget 127.0.0.1 CarlaBruni.mp3
+    # LFTP lsend 127.0.0.1 CarlaBruni.mp3
     cmd = input()
     match = pattern.match(cmd)
     if match:
--- LFTPServer.py
+++ LFTPServer.py
@@ -9,11 +9,10 @@ SERVER_FOLDER = 'ServerFiles/'
 # 接收到lget命令，向客户端发送文件
 def lget(server_socket, client_address, large_file_name):
     print(large_file_name)
-
-    # 发送数据包次数计数
-    pkt_count = 0
     # 模式rb 以二进制格式打开一个文件用于只读。文件指针将会放在文件的开头。
     file_to_send = open(SERVER_FOLDER + large_file_name, 'rb')
+    # 发送数据包次数计数
+    pkt_count = 0
 
     # 用缓冲区循环发送文件
     while True:
@@ -26,10 +25,40 @@ def lget(server_socket, client_address, large_file_name):
             break
         # 等待客户端ACK
         data, client_address = server_socket.recvfrom(BUF_SIZE)
-        # print('接受自 ', client_address, ' 收到数据为 : ', data.decode('utf-8'))
+        # print('接受自 ', client_address, '收到数据为 : ', data.decode('utf-8'))
         pkt_count += 1
 
+    file_to_send.close()
     print(large_file_name, '发送完毕，发送数据包的数量：' + str(pkt_count))
+
+
+# 接收到lsend命令，客户端向服务端发送文件
+def lsend(server_socket, client_address, large_file_name):
+    print(large_file_name)
+    # 创建文件。模式wb 以二进制格式打开一个文件只用于写入。如果该文件已存在则打开文件，
+    # 并从开头开始编辑，即原有内容会被删除。如果该文件不存在，创建新文件。
+    file_to_recv = open(SERVER_FOLDER + large_file_name, 'wb')
+    # 接收数据包次数计数
+    pkt_count = 0
+
+    # 发送接收允许
+    server_socket.sendto('接收允许'.encode('utf-8'), client_address)
+
+    # 开始接收数据包
+    while True:
+        # 用缓冲区接收文件
+        data, client_address = server_socket.recvfrom(BUF_SIZE)
+        if str(data) != "b'end'":  # 'end'为结束标志
+            file_to_recv.write(data)
+        else:
+            break  # 接受到结束通知,结束循环
+        # 向客户端发送ACK
+        server_socket.sendto('ACK'.encode('utf-8'), client_address)
+        pkt_count += 1
+
+    file_to_recv.close()
+    print('成功接收的数据包数量：' + str(pkt_count))
+
 
 def server_listening(server_socket):
     # 三次握手
@@ -40,19 +69,33 @@ def server_listening(server_socket):
     # 来自客户端的命令，格式为[lsend|lget]#large_file_name，因此文件命名不允许含有#
     cmd = message.decode('utf-8').split('#')[0]
     large_file_name = message.decode('utf-8').split('#')[1]
-    # 若命令是lget且文件不存在
-    if cmd == 'lget' and (os.path.exists(SERVER_FOLDER + large_file_name) is False):
-        server_socket.sendto('fileNotExists'.encode('utf-8'), client_address)
-        return
-
-    # 连接允许
-    server_socket.sendto('连接允许'.encode('utf-8'), client_address)
-    # 等待ACK
-    message, client_address = server_socket.recvfrom(BUF_SIZE)
-    print('来自', client_address, '的数据是: ', message.decode('utf-8'))
 
     if cmd == 'lget':
+        # 文件不存在，并告知客户端
+        if os.path.exists(SERVER_FOLDER + large_file_name) is False:
+            server_socket.sendto('fileNotExists'.encode('utf-8'), client_address)
+            return
+
+        # TODO: 在此要把各样工作(如分配线程等)准备好，再发送连接允许
+
+        # 连接允许
+        server_socket.sendto('连接允许'.encode('utf-8'), client_address)
+        # 等待ACK
+        message, client_address = server_socket.recvfrom(BUF_SIZE)
+        print('来自', client_address, '的数据是: ', message.decode('utf-8'))
+
         lget(server_socket, client_address, large_file_name)
+    elif cmd == 'lsend':
+        # 连接允许
+        server_socket.sendto('连接允许'.encode('utf-8'), client_address)
+        # 等待ACK
+        message, client_address = server_socket.recvfrom(BUF_SIZE)
+        print('来自', client_address, '的数据是: ', message.decode('utf-8'))
+
+        # TODO: 在此要把各样工作(如分配线程等)准备好，再发送接收允许(在lsend内)
+
+        lsend(server_socket, client_address, large_file_name)
+
 
 def main():
     # 检查接收文件夹是否存在
